9e2b8374fabe007a480be24dd8972d2651f2af22
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-10-23 18:27:04 +0530

    frontend file added

--- Frontend/src/components/home/Features.jsx
+++ Frontend/src/components/home/Features.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Zap } from "lucide-react";
+import Title from './Title';
 const Features = () => {
     const [isHover, setIsHover] = React.useState(false);
   return (
     < div id='features' className='flex flex-col items-center my-10 scroll-m-12'>
 
-      <div class="flex items-center gap-2 text-sm text-green-600 bg-green-400/10  rounded-full px-4 py-1.5">
+      <div className="flex items-center gap-2 text-sm text-green-600 bg-green-400/10  rounded-full px-4 py-1.5">
        <Zap width={14} />
     <span>Simple Process</span>
        </div>
+       <Title title='Build your resume' description='Our streamlined process helps you create a professional resume in minutes with intelligent AI-powered tools and features .'/>
 
-            <div className="flex flex-col md:flex-row items-center justify-center">
+            <div className="flex flex-col md:flex-row items-center justify-center xl:-mt-10">
                 <img className="max-w-2xl w-full xl:-ml-32" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/features/group-image-1.png" alt="" />
                 <div className="px-4 md:px-0" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
                     <div className={"flex items-center justify-center gap-6 max-w-md group cursor-pointer"}>
