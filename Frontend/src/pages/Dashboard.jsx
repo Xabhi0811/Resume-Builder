949b40023b51011d702e38a8d286a24559ce1a5e
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-10-24 03:07:01 +0530

    lucifer

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,23 @@
-import { PlusIcon, UploadCloudIcon } from 'lucide-react'
-import React, { useState } from 'react'
+import { FilePenLineIcon, PencilIcon, PlusIcon, TrashIcon, UploadCloudIcon, XIcon } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
 import{dummyResumeData} from '../assets/assets'
 
 const Dashboard = () => {
+  const colors = ["#9333ea" , "#d97706", "#dc2626", "#0284c7", "#16a34a"]
   const [allResumes , setAllResumes] =useState([])
+  const [ShowCreateResume , setShowCreateResume] =useState(false)
+  const [ShowUploadResume , setShowUploadResume] =useState(false)
+  const [title , setTitle] =useState('')
+  const [resume , setResume] =useState(null)
+  const [editResumeId , setEditResumeId] =useState('')
 
   const loadAllResumes = async() =>{
     setAllResumes(dummyResumeData)
   }
+
+  useEffect(()=>{
+    loadAllResumes()
+  },[])
   return (
     <div>
       <div className='max-w-7xl mx-auto px-4 py-8'>
@@ -42,7 +52,60 @@ const Dashboard = () => {
        </div>
        
        <hr className='border-slate-300 my-6 sm:w-[305px]'/>
+        
+        <div className='grid grid-cols-2 sm:flex flex-wrap gap-4'>
+           {allResumes.map((resume , index)=>{
+             const baseColor = colors[index %colors.length]
+             return(
+              <button key={index} className='relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center 
+              rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer' style=
+              {{background:`linear-gradient(135deg, ${baseColor}10),${baseColor}40)
+               `,borderColor: baseColor +'40'}}>
+                
+                <FilePenLineIcon className='size-7 group-hover:scale-105
+                transition-all' style={{color: baseColor}}/>
+                <p className='text-sm group-hover:scale-105 transition-all
+                 px-2 text-center' style={{color: baseColor}}>
+                  {resume.title}</p>
+                  <p className='absolute bottom-1 text-[11px] text-slate-400
+                  group-hover:text-slate-500 transition-all duration-300 px-2
+                  text-center'style={{color: baseColor + '90'}}>
+                    updated on {new Date(resume.updatedAt).toLocaleDateString()}
+                  </p>
+                  <div className='absolute top-1 right-1 group-hover:flex items-center
+                  hidden'>
+                    <TrashIcon className='size-7 p-1.5 hover:bg-white/50 rounded
+                    text-slate-700 transition-colors'/>
+                    <PencilIcon className='size-7 p-1.5 hover:bg-white/50 rounded
+                    text-slate-700 transition-colors'/>
+                  </div>
+              </button>
+             )
+           })}
+        </div>
 
+        <div>
+          {ShowCreateResume &&(
+            <form className='fixed inset-0
+             bg-black/70 backdrop-blur bg-opacity-50 z-10 flex 
+             items-center justify-center'>
+              <div className="">
+                <h2>Create a Resume</h2>
+                <input type='text' placeholder='Enter resume title' className='w-full
+                px-4 py-2 mb-4 focus:border-green-600' required />
+                <button className='w-full py-2 bg-green-600 text-white
+                 rounded hover:bg-green-700 transition-colors'>Create Resume</button>
+                 <XIcon className='absolute top-4 right-4 text-slate-400
+                 hover:text-slate-600 cursor-pointer transition-colors' onClick={()=>
+                  {setShowCreateResume(false); setTitle('')}} />
+
+                
+              </div>
+             </form>
+          )
+          }
+
+        </div>
       </div>      
     </div>
   )
