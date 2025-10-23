6a4e05395c8ec53c1d309cabe2412bfa5be3ec5a
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-10-24 03:38:36 +0530

    icon

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -101,7 +101,7 @@ const Dashboard = () => {
               <div onClick={e=>e.stopPropagation()} className="relative bg-slate-50 border shadow-md
               rounded-lg w-full max-w-sm p-6">
                 <h2 className='text-xl font-bold mb-4'>Create a Resume</h2>
-                <input type='text' placeholder='Enter resume title' className='w-full
+                <input  onChange={(e)=>setTitle(e.target.value)} value={title} type='text' placeholder='Enter resume title' className='w-full
                 px-4 py-2 mb-4 focus:border-green-600' required />
                 <button className='w-full py-2 bg-green-600 text-white
                  rounded hover:bg-green-700 transition-colors'>Create Resume</button>
@@ -113,6 +113,30 @@ const Dashboard = () => {
               </div>
              </form>
           )
+          }
+
+          {  
+             ShowUploadResume &&(
+                <form onSubmit={uploadResume} onClick={()=>setShowUploadResume(false)} className='fixed inset-0
+             bg-black/70 backdrop-blur bg-opacity-50 z-10 flex 
+             items-center justify-center'>
+              <div onClick={e=>e.stopPropagation()} className="relative bg-slate-50 border shadow-md
+              rounded-lg w-full max-w-sm p-6">
+                <h2 className='text-xl font-bold mb-4'>Upload  Resume</h2>
+                <input onChange={(e)=>setTitle(e.target.value)} value={title} type='text' placeholder='Enter resume title' className='w-full
+                px-4 py-2 mb-4 focus:border-green-600' required />
+                <button className='w-full py-2 bg-green-600 text-white
+                 rounded hover:bg-green-700 transition-colors'>Upload resume</button>
+                 <XIcon className='absolute top-4 right-4 text-slate-400
+                 hover:text-slate-600 cursor-pointer transition-colors' onClick={()=>
+                  {setShowUploadResume(false); setTitle('')}} />
+
+                
+              </div>
+             </form>
+
+             )
+
           }
 
         </div>
