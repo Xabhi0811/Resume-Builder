2d53427cca4b7aff099cc1a910f90962587ac527
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-10-24 20:32:43 +0530

    yrr kaam bahut hai

--- Frontend/src/pages/ResumeBuilder.jsx
+++ Frontend/src/pages/ResumeBuilder.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets'
+import { Link } from 'lucide-react'
 
 const ResumeBuilder = () => {
+
+  const {resumeId} = useParams
+  const [resumeData , setResumeData] = useState({
+    _id: '',
+    title: '',
+    personal_info:{},
+    professional_summary:"",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#2B82F6",
+    public: false,
+  })
+
+  const loasExistingResume =async () =>{
+    const resume = dummyResumeData.find(resume => resume.id == resumeId)
+    if(resume){
+      setResumeData(resume)
+      document.title = resume.title
+    }
+  }
+
+  useEffect(()=>{
+    loasExistingResume()
+  },[])
+   
   return (
     <div>
-      <h1>Resume</h1>
+     <div className="">
+      <Link />
+     </div>
     </div>
   )
 }
