c8f31ff155e33a1ad6f8a2a3331ab5d66c674f23
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-10-25 02:56:47 +0530

    3 bja gye hai ek problem ho gai hai image default set ho gai hai

--- Frontend/src/components/PersonalInfoForm.jsx
+++ Frontend/src/components/PersonalInfoForm.jsx
@@ -2,6 +2,11 @@ import { User } from 'lucide-react'
 import React from 'react'
 
 const PersonalInfoForm = ({data , onChange, removeBackground, setRemoveBackground}) => {
+     
+  const handleChange =(field, value)=>{
+     onChange({...data, [field]: value})
+  }
+  
   return (
     <div>
       <h3 className='text-lg font-semibold text-gray-900'>Personal Infromation</h3>
@@ -10,8 +15,8 @@ const PersonalInfoForm = ({data , onChange, removeBackground, setRemoveBackgroun
       <label>
         {data.image ? (
           <img src={typeof data.image === 'string' ? data.image :
-            URL. createObjectURL(data.image)
-          } alt=' user-image' className='w-16 h-16
+            URL.createObjectURL(data.image)
+          } alt='user-image' className='w-16 h-16
           rounded-full object-cover mt-5 ring ring-slate-300
           hover:opacity-80' />
         ) :(
@@ -21,8 +26,26 @@ const PersonalInfoForm = ({data , onChange, removeBackground, setRemoveBackgroun
            upload user image
           </div>
         )}
-        <input type='file' accept='image/jpeg, image/png' className='hidden' />
+        <input type='file' accept='image/jpeg, image/png' className='hidden' 
+        onChange={(e)=>handleChange("image", e.target.files[0])}/>
       </label>
+      {typeof data.image === 'object' && (
+        <div className='flex flex-col gap-1 pl-4 text-sm'>
+          <p>Remove Background</p>
+          <label className='relative inline-flex items-center cursor-pointer
+          text-gray-900 gap-3'>
+            <input type="checkbox" className="sr-only peer" onChange={()=>
+              setRemoveBackground(prev =>!prev)
+            } checked={removeBackground} />
+            <div className="w-9 h-5 bg-slate-300 rounded-full peer
+            peer-checked:bg-green-600 transition-colors duration-200">
+            </div>
+            <span className='dot absolute left-1 top-1 w-3 h-3 bg-white
+            rounded-full transition-transform duration-200
+             ease-in-out peer-checked:translate-x-4'></span>
+          </label>
+          </div>
+      )}
      </div>
     </div>
   )
