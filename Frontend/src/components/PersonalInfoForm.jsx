3de686c7f83d6c677100301900cbf6e44cb825f7
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-10-25 00:47:41 +0530

    aaj bahut neend a rhi hai

---
+++ Frontend/src/components/PersonalInfoForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PersonalInfoForm = ({data , onChange, removeBackground, setRemoveBackground}) => {
+  return (
+    <div>
+      <h3 className='text-lg font-semibold text-gray-900'>Personal Infromation</h3>
+      <p className='text-sm text-gray-600'> Get Startedd with the personal information</p>
+    </div>
+  )
+}
+
+export default PersonalInfoForm
