82302aadfc73074a3a5d57b5e3dd0e32d78a70e6
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-10-27 02:18:49 +0530

    skills

--- Frontend/src/pages/ResumeBuilder.jsx
+++ Frontend/src/pages/ResumeBuilder.jsx
@@ -10,6 +10,7 @@ import ProfessionSummaryForm from '../components/ProfessionSummaryForm'
 import ExperienceForm from '../components/ExperienceForm'
 import EducationFrom from '../components/EducationFrom'
 import ProjectForm from '../components/ProjectForm'
+import SkillsForm from '../components/SkillsForm'
 
 
 const ResumeBuilder = () => {
@@ -136,7 +137,20 @@ const ResumeBuilder = () => {
                   onChange={(data)=>setResumeData(prev=> ({...prev ,project: data}))}
                   />
                 ) }
+
+
+                
+                 { activeSection.id === 'skills' && (
+                  <SkillsForm data={resumeData.skills}
+                  onChange={(data)=>setResumeData(prev=> ({...prev ,skills: data}))}
+                  />
+                ) }
              </div>
+             <button className='bg-gradient-to-br from-green-100 to-green-200
+             ring-green-300 text-green-600 ring hover:ring-green-400
+             transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+              Save Changes
+             </button>
         </div>
         </div>
         {/*Right panel-Preview */}
