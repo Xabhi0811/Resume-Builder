13dadf037842e387ac60b63321c24b624047bf85
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-10-27 23:57:06 +0530

    preview

--- Frontend/src/pages/Preview.jsx
+++ Frontend/src/pages/Preview.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets'
 
 const Preview = () => {
+  const {resumeId} =useParams()
+
+  const [resumeData , setResumeData] = useState(null)
+
+  const loadResume = async () =>{
+    setResumeData(dummyResumeData.find(resume => resume._id === resumeId))
+  }
   return (
     <div>
       preview
