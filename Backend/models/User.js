ab008bcab0d5e32c4feabf79719ec87dcdbce795
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-10-30 00:15:21 +0530

    usercontroller

---
+++ Backend/models/User.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+      lowercase: true,
+      match: [/.+\@.+\..+/, "Please enter a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minlength: [6, "Password must be at least 6 characters"],
+    },
+  },
+  {
+    timestamps: true, 
+  }
+ 
+);
+
+
+ UserSchema.methods.comparePassword = function (password){
+    return bcrypt.compareSync(password, this.password)
+ }
+
+
+
+const User = mongoose.model("User", UserSchema);
+export default User;
