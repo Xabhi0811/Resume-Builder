2c563d6e196a07d546a41e12623bb59ee80cb0aa
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-10-30 04:23:33 +0530

    imgaekit add kar di

--- Backend/Controller/resumeController.js
+++ Backend/Controller/resumeController.js
@@ -1,4 +1,6 @@
+import imagekit from "../config/imageKit";
 import Resume from "../models/Resume";
+import fs from 'fs'
 
 export const createResume = async (req, res)=>{
     try {
@@ -74,7 +76,33 @@ export const updateResume = async (req, res) =>{
     try {
          const userId = req.userId;
          const {resumeId , resumeData, removeBackground} = req.body
+         const image = req.file;
+
+         let resumeDataCopy =JSON.parse(resumeData)
+
+         if(image){
+
+            const imageBufferData =fs.createReadStream(image.path)
+            const response = await imagekit.files.upload({
+  file: imageBufferData,
+  fileName: 'resume.png',
+  folder: 'user-resumes',
+  transformation:{
+    pre: 'w-300, h-300, fo-face, z-0.75' +(
+        removeBackground ? ',e-bgremove' : ''
+    )
+  }
+
+});
+
+resumeDataCopy.personal_info.image = response.url
+
+
+         }
+     const resume = await Resume.findByIdAndUpdate({userId, _id: resumeId}, resumeDataCopy,{new: true})
+
+     return res.status(200).json({message: 'Saved successfully', resume})
     } catch (error) {
-        
+        return res.status(400).json({message: error.message})
     }
 }
