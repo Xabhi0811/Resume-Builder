c45678e56d8588d6aed7687860a6ae83957750a4
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-11-01 17:52:54 +0530

    backend compelted

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config"
 import connectDB from "./config/db.js";
 import userRouter from "./routes/userRoutes.js";
 import resumeRouter from "./routes/resumeRoutes.js";
+import aiRouter from "./routes/aiRoutes.js";
 
 
 const app = express();
@@ -21,6 +22,7 @@ res.send("Server is live....")
 })
 app.use('/api/users', userRouter)
 app.use('/api/resumes', resumeRouter)
+app.use('/api/ai', aiRouter)
 
 app.listen(PORT,()=>{
     console.log(`Server is running on Port ${PORT}`)
