f0eb7188dd1539d8eb8e785ff5ef0d6227ef4c06
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-11-01 19:45:47 +0530

    login form

--- Frontend/src/app/store.js
+++ Frontend/src/app/store.js
@@ -1,4 +1,9 @@
 
+import{configureStore} from '@reduxjs/toolkit'
+import authReducer from "./features/authSlice"
 
-
-export const store = congi
+export const store = configureStore({
+     reducer: {
+        auth :  authReducer 
+     }  
+})
