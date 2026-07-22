c05f7e3c3a2f37d06853976c6f3dc692ff6acac0
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-07-23 00:41:05 +0530

    delopyment

--- Frontend/src/app/store.js
+++ Frontend/src/app/store.js
@@ -3,8 +3,6 @@ import{configureStore} from '@reduxjs/toolkit'
 import authReducer from "./features/authSlice"
 
 
-
-
 export const store = configureStore({
      reducer: {
         auth :  authReducer 
